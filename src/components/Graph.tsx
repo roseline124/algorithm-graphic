113ccae0e17a4bd8410b460a700d5b5bd63fd40c
roseline124 <guseod24@gmail.com>
2021-05-01 19:21:28 +0900

    :sparkles: add graph with d3 force layout

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,11 +1,14 @@
-import tw from 'twin.macro'
-import { FC, ReactNode, useEffect, useRef } from 'react'
 import * as d3 from 'd3'
+import tw, { css } from 'twin.macro'
+import flatten from 'lodash/flatten'
+import uniqBy from 'lodash/uniqBy'
+import { FC, ReactNode, useEffect, useRef, useState } from 'react'
 
-import { Edge } from 'algorithms/search/bfs'
+const CONTAINER_WIDTH = 400
+const CONTAINER_HEIGHT = 300
 
 interface GraphProps {
-  rootNode: ReactNode
+  rootNode: GraphNode
   edges: Edge[]
 }
 
@@ -13,36 +16,96 @@ const Root = tw.div`
   flex items-center justify-center
 `
 
-const Graph: FC<GraphProps> = ({ rootNode, edges }) => {
-  const myRef = useRef(null)
-  const dataset = [100, 200, 300, 400, 500]
+const lineStyle = css`
+  line {
+    stroke: #ccc;
+  }
+`
 
-  console.log({ myRef })
+const Graph: FC<GraphProps> = ({ rootNode, edges }) => {
+  const nodes = uniqBy(flatten(edges), 'value')
+  const links: d3.GraphLink[] = edges.map(edge => ({
+    source: nodes.findIndex(node => node.value === edge[0].value),
+    target: nodes.findIndex(node => node.value === edge[1].value),
+  }))
+
+  const [nodeGroup, setNodeGroup] = useState(null)
+  const [linkGroup, setLinkGroup] = useState(null)
+
+  const nodeRef = useRef(null)
+  const linkRef = useRef(null)
+
+  let u
+  const updateLinks = () => {
+    u = linkGroup.selectAll('line').data(links)
+
+    u.enter()
+      .append('line')
+      .merge(u)
+      .attr('x1', function (d) {
+        return d.source.x
+      })
+      .attr('y1', function (d) {
+        return d.source.y
+      })
+      .attr('x2', function (d) {
+        return d.target.x
+      })
+      .attr('y2', function (d) {
+        return d.target.y
+      })
+
+    u.exit().remove()
+  }
+  const updateNodes = () => {
+    u = nodeGroup.selectAll('text').data(nodes)
+
+    u.enter()
+      .append('text')
+      .text(function (d) {
+        return d.value
+      })
+      .merge(u)
+      .attr('x', function (d) {
+        return d.x
+      })
+      .attr('y', function (d) {
+        return d.y
+      })
+      .attr('dy', function (d) {
+        return 5
+      })
+
+    u.exit().remove()
+  }
+
+  const ticked = () => {
+    if (nodeGroup && linkGroup) {
+      updateLinks()
+      updateNodes()
+    }
+  }
+
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force('charge', d3.forceManyBody().strength(-220))
+    .force('center', d3.forceCenter(CONTAINER_WIDTH / 2, CONTAINER_HEIGHT / 2))
+    .force('link', d3.forceLink().links(links))
+    .on('tick', ticked)
 
   useEffect(() => {
-    const size = 500
-    const svg = d3
-      .select(myRef.current)
-      .append('svg')
-      .attr('width', size)
-      .attr('height', size)
+    setNodeGroup(d3.select(nodeRef.current))
+    setLinkGroup(d3.select(linkRef.current))
 
-    const rect_width = 95
-    svg
-      .selectAll('rect')
-      .data(dataset)
-      .enter()
-      .append('rect')
-      .attr('x', (d, i) => 5 + i * (rect_width + 5))
-      .attr('y', (d: number) => size - d)
-      .attr('width', rect_width)
-      .attr('height', d => d)
-      .attr('fill', 'teal')
+    simulation.force('link', d3.forceLink().links(links))
   }, [])
 
   return (
     <Root tw="p-18">
-      <div ref={myRef}></div>
+      <svg width={CONTAINER_WIDTH} height={CONTAINER_HEIGHT}>
+        <g id="links" ref={linkRef} css={lineStyle}></g>
+        <g id="nodes" ref={nodeRef}></g>
+      </svg>
     </Root>
   )
 }
--- src/pages/bfs.tsx
+++ src/pages/bfs.tsx
@@ -1,14 +1,24 @@
 import { FC } from 'react'
 import 'twin.macro'
-
-import Node from 'components/Node'
+// import Node from 'components/Node'
 import Graph from 'components/Graph'
+
+import flatten from 'lodash/flatten'
+import uniqBy from 'lodash/uniqBy'
 
 // Breadth-First Search
 const BFS: FC = () => {
+  const edges: Edge[] = [
+    [{ value: 1 }, { value: 2 }],
+    [{ value: 1 }, { value: 3 }],
+    [{ value: 1 }, { value: 4 }],
+    [{ value: 2 }, { value: 4 }],
+    [{ value: 3 }, { value: 4 }],
+  ]
+
   return (
     <div>
-      <Graph rootNode={1000} edges={[[1000, 999]]} />
+      <Graph rootNode={{ value: 1000 }} edges={edges} />
     </div>
   )
 }
